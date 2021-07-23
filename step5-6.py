bf417ac0f591eb5b29fb2b7ecaeeb93107f766a6
dirn5454 <dirn5454@naver.com>
2021-07-23 16:45:04 +0900

    step5 finish

--- step5-6.py
+++ step5-6.py
@@ -1,3 +1,16 @@
 n = int(input())
+arr =[]
 for i in range(n):
-    
+    arr = input()
+    sum = 0
+    c = 1
+    for i in arr:
+        if i == 'O':
+            sum += c
+            c += 1
+        else:
+            c=1
+    print(sum)
+
+
+
